271771f4cea4d6708ba07dc37995cebef57c252b
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-01-29 14:33:34 +0700

    feat: add logs

--- src/server/gists.ts
+++ src/server/gists.ts
@@ -1,60 +1,69 @@
 const Octokit = require("@octokit/rest");
 
 const octokit = new Octokit({
-  auth: process.env.GIST_TOKEN
+	auth: process.env.GIST_TOKEN
 });
-const GIST_ID = process.env.GIST_ID;
 
 export const updateGist = async (name: string, device: string, response: any) => {
 	const todayDate = new Date().toISOString().substring(0, 10);
 
 	const lastData = await octokit.gists.get({
-		gist_id: GIST_ID,
-	})
+		gist_id: process.env.GIST_ID,
+	});
 
-	const objectData = JSON.parse(lastData);
-	let newData = objectData[todayDate];
-
-	if (!newData) {
-		newData = [
-			{
-				name: name.toLowerCase(),
-				[device] : {
-					response,
-				}
-			}
-		]
-	} else if (newData) {
-		const existing = newData.find(i => i.name.toLowerCase() === name.toLowerCase());
-		if (existing) {
-			const woExisting = newData.filter(i => i.name.toLowerCase() !== name.toLowerCase());
-			newData = [
-				...woExisting,
-				{
-					name: name.toLowerCase(),
-					...existing,
-					[device] : {
-						response,
-					}
-				}
-			];
-		} else {
-			newData = [
-				...newData,
-				{
-					name: name.toLowerCase(),
-					[device] : {
-						response,
-					}
-				}
-			];
-		}
+	if (lastData) {
+		console.log('> [GIST] - get existing data...');
 	}
 
-	await octokit.gists.update({
-		gist_id: GIST_ID,
-		files: {
-			content: {...objectData, ...{[todayDate]: newData} }
+	try {
+		const objectData = JSON.parse(lastData);
+		let newData = objectData[todayDate];
+
+		if (!newData) {
+			newData = [
+				{
+					name: name.toLowerCase(),
+					[device]: {
+						response,
+					}
+				}
+			]
+		} else if (newData) {
+			const existing = newData.find(i => i.name.toLowerCase() === name.toLowerCase());
+			if (existing) {
+				const woExisting = newData.filter(i => i.name.toLowerCase() !== name.toLowerCase());
+				newData = [
+					...woExisting,
+					{
+						name: name.toLowerCase(),
+						...existing,
+						[device]: {
+							response,
+						}
+					}
+				];
+			} else {
+				newData = [
+					...newData,
+					{
+						name: name.toLowerCase(),
+						[device]: {
+							response,
+						}
+					}
+				];
+			}
 		}
-	})
+
+		console.log('> [GIST] - updating value...');
+		await octokit.gists.update({
+			gist_id: process.env.GIST_ID,
+			files: {
+				content: { ...objectData, ...{ [todayDate]: newData } }
+			}
+		})
+
+	} catch (e) {
+		console.error('> Error gist', e);
+	}
 }
--- src/server/job.ts
+++ src/server/job.ts
@@ -4,67 +4,84 @@ import { updateGist } from './gists';
 import { EcommerceItem } from '../index';
 
 const run = async (name: string, url: string, device: string): Promise<any | null> => {
-  const URL = `https://builder-dot-lighthouse-ci.appspot.com/ci`;
+	const URL = `https://builder-dot-lighthouse-ci.appspot.com/ci`;
 
-  try {
-		// @ts-ignore
-    const resp = await fetch(URL, {
-			method: 'POST',
+	let isSecretNotFound = false;
+	if (process.env.LIGHTHOUSE_API_KEY) {
+		console.log(`> Found env LIGHTHOUSE_API_KEY`);
+	} else {
+		isSecretNotFound = true;
+		console.error(`> env LIGHTHOUSE_API_KEY not found`);
+	}
+
+	if (process.env.GIST_TOKEN) {
+		console.log(`> Found env GIST_TOKEN`);
+	} else {
+		isSecretNotFound = true;
+		console.error(`> env GIST_TOKEN not found`);
+	}
+
+	if (process.env.GIST_ID) {
+		console.log(`> Found env GIST_ID: ${process.env.GIST_ID}`);
+	} else {
+		isSecretNotFound = true;
+		console.error(`> env GIST_ID not found`);
+	}
+
+	if (!isSecretNotFound) {
+		try {
 			// @ts-ignore
-      headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json',
-				'X-API-KEY': process.env.LIGHTHOUSE_API_KEY,
-			},
-			body: JSON.stringify({
-				output: 'json',
-				url: url,
-				emulatedFormFactor: device
-			})
-		});
+			const resp = await fetch(URL, {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+					'X-API-KEY': process.env.LIGHTHOUSE_API_KEY,
+				},
+				body: JSON.stringify({
+					output: 'json',
+					url: url,
+					emulatedFormFactor: device,
+				})
+			});
 
-    const result = await resp.json();
-    if (result) {
+			const result = await resp.json();
 
-			const categories = result?.categories || null;
-			const audits = result?.audits || null;
-			const resourceItems = audits?.['resource-summary']?.details?.items || [];
+			if (result) {
+				const categories = result?.categories || null;
+				const audits = result?.audits || null;
+				const resourceItems = audits?.['resource-summary']?.details?.items || [];
 
-			const response = {
-				perf: categories?.performance?.score || 0,
-				aiiy: categories?.accessibility?.score || 0,
-				pwa: categories?.pwa?.score || 0,
+				const response = {
+					perf: categories?.performance?.score || 0,
+					aiiy: categories?.accessibility?.score || 0,
+					pwa: categories?.pwa?.score || 0,
 
-				fcp: audits['first-contentful-paint']?.numericValue || 0,
-				ttfb: audits['time-to-first-byte']?.numericValue || 0,
-				tti: audits['interactive']?.numericValue || 0,
-				si: audits['speed-index']?.numericValue || 0,
+					fcp: audits['first-contentful-paint']?.numericValue || 0,
+					ttfb: audits['time-to-first-byte']?.numericValue || 0,
+					tti: audits['interactive']?.numericValue || 0,
+					si: audits['speed-index']?.numericValue || 0,
 
-				reqCount: resourceItems?.[0]?.requestCount || 0,
-				reqSize: resourceItems?.[0]?.size || 0,
+					reqCount: resourceItems?.[0]?.requestCount || 0,
+					reqSize: resourceItems?.[0]?.size || 0,
+				}
+
+				console.log(`> Result for ${url}: ${JSON.stringify(response)} \n\n`);
+				updateGist(name, device, response);
+				return response;
 			}
+		} catch (e) {
+			console.error('> Error job', e);
+		}
+	}
 
-			console.log(`> Result for ${url}`, JSON.stringify(response));
-
-			updateGist(name, device, response);
-    }
-  } catch (e) {
-    console.error('> Error', e);
-  }
-
-  return null;
+	return null;
 }
 
 const readData = () => {
-	data.map((item :EcommerceItem, idx: number) => {
-		setTimeout(() => {
-			run(item.name, item.urlMobile, 'mobile');
-
-			setTimeout(() => {
-				run(item.name, item.urlDesktop, 'desktop');
-			}, idx * 3000);
-
-		}, idx * 3000);
+	data.map(async (item: EcommerceItem) => {
+		await run(item.name, item.urlMobile, 'mobile');
+		await run(item.name, item.urlDesktop, 'desktop');
 	})
 }
 
