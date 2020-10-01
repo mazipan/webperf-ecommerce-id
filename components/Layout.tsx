8f2aca9d971d91c2170a4b44b33bdb9915fc1a24
Irfan Maulana <mazipanneh@gmail.com>
2020-10-01 23:47:28 +0700

    fix: change meta theme

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,7 +15,7 @@ const Layout = ({ children }): React.ReactElement => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-        <meta name="theme-color" content="#7B341E" />
+        <meta name="theme-color" content="#000000" />
         <title key="title">{title}</title>
         <meta key="description" name="description" content={desc} />
 
