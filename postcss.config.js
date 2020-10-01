33a5f5283c7752f70b04d70a5ad61e90a89c73c7
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-10-01 21:49:46 +0700

    fix: purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 const purgecss = [
   '@fullhuman/postcss-purgecss',
   {
-    content: ['./components/**/*.js', './pages/**/*.js'],
+    content: ['./components/**/*.{js,ts,tsx}', './pages/**/*.{js,ts,tsx}'],
     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
 ];
